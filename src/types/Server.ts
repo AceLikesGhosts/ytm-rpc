113e2271dd5ac55ae56cb7d2e6aa6f401b5b91c1
AceLikesGhosts <acelikesghosts@gmail.com>
2023-08-16 07:18:58 -0700

    chore(server): remove unneeded type

--- src/types/Server.ts
+++ src/types/Server.ts
@@ -1,9 +1,4 @@
-import type { Globals } from './Globals';
 import type { Presence } from 'discord-rpc';
-
-export interface ServerConstructor {
-    new(options: Globals): Server;
-}
 
 export interface Server {
     update(presence: Presence): void;
