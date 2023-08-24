eb4591e6f363b570e4363ff4c8903db3d78d7482
AceLikesGhosts <acelikesghosts@gmail.com>
2023-08-23 21:49:46 -0700

    bat equal of previous commit

--- scripts/streamline.bat
+++ scripts/streamline.bat
@@ -1,32 +1,30 @@
 @echo off
+setlocal
 
-REM These are the stupid requirements for TypeScript, this isn't TypeScript.
+REM These are the equivalent commands for TypeScript (although it doesn't make sense in BAT).
+set "INSTALL_COMMAND=npm ci --quiet"
+set "BUILD_COMMAND=npm --silent run ts:build"
 
-REM Define commands as associative array
-setlocal enabledelayedexpansion
-set "COMMANDS[install]=npm ci"
-set "COMMANDS[build]=npm run ts:build"
-
-REM Function to install Replugged
+REM Install Replugged function (equivalent BAT function)
 :installReplugged
 (
   REM WARNING: this is hardcoded, seethe!
-  call :runGeneric "npm run build" "Finished transpiling Replugged plugin." "%~dp0..\client-mods\replugged"
-)
-goto :eof
+  call :runGeneric "npm run --silent build" "Finished transpiling Replugged plugin." "%~dp0..\client-mods\replugged"
+) &
 
-REM Function to get BetterDiscord plugin folder
+REM Function to get BetterDiscord plugin folder (equivalent BAT function)
 :getBetterDiscordPluginFolder
-if "%PROCESSOR_ARCHitecture%"=="AMD64" (
+if "%PROCESSOR_ARCH%"=="AMD64" (
   set "pluginFolder=%USERPROFILE%\AppData\Roaming\BetterDiscord\plugins"
 ) else (
   set "pluginFolder=%USERPROFILE%\.config\BetterDiscord\plugins"
 )
-echo !pluginFolder!
 goto :eof
 
-REM Function to install BetterDiscord
+REM Install BetterDiscord function (equivalent BAT function)
 :installBetterDiscord
+set "pluginFolder="
+call :getBetterDiscordPluginFolder
 set "bdPluginFolder=%~dp0..\client-mods\BetterDiscord"
 set "PLUGIN=%bdPluginFolder%\YTM.plugin.js"
 
@@ -34,23 +32,24 @@ if exist "%pluginFolder%" if exist "%PLUGIN%" if exist "%bdPluginFolder%" (
   copy "%PLUGIN%" "%pluginFolder%"
   echo ✔ Installed BetterDiscord plugin.
 ) else (
-  echo The installation script does not have enough permissions to access a required folder to copy the BetterDiscord plugin. 1>&2
+  echo The installation script does not have enough permissions to access a required folder to copy the BetterDiscord plugin. >&2
 )
 goto :eof
 
-REM Function to run generic commands
+REM Function to run commands (equivalent BAT function)
 :runGeneric
 set "command=%~1"
 set "finish=%~2"
 set "cwd=%~3"
+
 (
   if defined cwd (
-    pushd "%cwd%" && call %command%
+    pushd "%cwd%" && %command%
   ) else (
-    call %command%
+    %command%
   )
 ) || (
-  echo Failed to run command. 1>&2
+  echo Failed to run command. >&2
   exit /b 1
 )
 
@@ -58,42 +57,56 @@ echo ✔ %finish%
 goto :eof
 
 REM Start streamlining script
-echo Starting streamlining script:
-echo You are still required to manually install the Chromium extension which enables
-echo the server to work! Installation steps can be seen on the Github's README!
-
 set "queuedThings="
+set "missingArgs=0"
 
-REM Process arguments
-for %%a in (%*) do (
-  set "arg=%%a"
-  if "!arg!"=="--deps" (
-    set "queuedThings=!queuedThings!call :runGeneric "!COMMANDS[install]!" "Finished installing Node (server) dependencies." "%~dp0.."
-  )
-  if "!arg!"=="--build" (
-    set "queuedThings=!queuedThings!call :runGeneric "!COMMANDS[build]!" "Finished transpiling server." "%~dp0.."
-  )
-  if "!arg:~0,8!"=="--client" (
-    set "client=!arg:~9!"
-    if "!client!"=="bd" (
-      set "queuedThings=!queuedThings!call :installBetterDiscord"
+for %%I in (%*) do (
+  set "arg=%%~I"
+  if /i "%arg%"=="--deps" (
+    call :runGeneric "%INSTALL_COMMAND%" "Finished installing Node (server) dependencies." "%~dp0.."
+  ) else if /i "%arg%"=="--build" (
+    call :runGeneric "%BUILD_COMMAND%" "Finished transpiling server." "%~dp0.."
+  ) else if /i "%arg:~0,9%"=="--client=" (
+    set "client=%arg:~9%"
+    if /i "!client!"=="bd" (
+      call :installBetterDiscord
+    ) else if /i "!client!"=="replugged" (
+      call :installReplugged
+    ) else (
+      echo Expected 'bd' | 'replugged' but got '!client!' >&2
+      exit /b 1
     )
-    if "!client!"=="replugged" (
-      set "queuedThings=!queuedThings!call :installReplugged"
-    )
+  ) else (
+    set "missingArgs=1"
   )
 )
 
-REM Execute queued commands
-if "!queuedThings!"=="" (
+if not defined queuedThings (
   echo Missing required arguments!
   echo This script supports the following arguments:
   echo --deps -> Install dependencies for the Node server.
   echo --build -> Build the TypeScript server.
   echo --client -> Install a client mod. Supported options are 'bd' and 'replugged'.
   echo Example: %~nx0 --deps --build --client=bd
-) else (
-  for %%q in (!queuedThings!) do (
-    %%q
-  )
+) else if "%missingArgs%"=="0" (
+  echo You are still required to manually install the Chromium extension which enables
+  echo the server to work! Installation steps can be seen on the Github's README!
 )
+
+for %%I in (%queuedThings%) do (
+  call %%I
+  call :wait
+)
+
+goto :eof
+
+REM Function to wait for background processes (equivalent BAT function)
+:wait
+for /l %%C in (1,1,1) do (
+  set "running="
+  for %%P in (%queuedThings%) do (
+    tasklist /fi "PID eq %%C" | find /i "cmd.exe" | find /i "%%P" >nul && set "running=1" && exit /b
+  )
+  if not defined running exit /b
+)
+goto :eof
